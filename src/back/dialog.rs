57796bae57d96207c2d1b6e37a45481b895757a2
Kaustubh Patange <developerkp16@gmail.com>
2021-03-16 15:38:23 +0530

    [Experimental] Native dialogs for linux & macos

--- src/back/dialog.rs
+++ src/back/dialog.rs
@@ -4,9 +4,17 @@ extern crate wfd;
 use self::wfd::{DialogError, DialogParams};
 
 #[cfg(any(target_os = "linux", target_os = "macos"))]
+use rfd::FileDialog;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub fn launch_windows_save_dialog() -> Result<String, ()> {
-    println!("- Error: Native dialogs are not supported on {}", std::env::consts::OS);
-    Err(())
+    let file = FileDialog::new().add_filter("png", &["png"]).save_file();
+    match file {
+        Some(buff) => {
+            Ok(buff.as_path().to_str().unwrap().to_owned())
+        }
+        None => { Err(()) }
+    }
 }
 
 #[cfg(target_os = "windows")]
